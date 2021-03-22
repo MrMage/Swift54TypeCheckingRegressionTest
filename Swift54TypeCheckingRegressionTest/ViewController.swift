475be304cd380ff7324f3af300ece11d1c9dde95
Daniel Alm <Daniel.Alm@ForumD.net>
2021-03-22 13:14:03 +0100

    Initial Commit

---
+++ Swift54TypeCheckingRegressionTest/ViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ViewController.swift
+//  Swift54TypeCheckingRegressionTest
+//
+//  Created by Daniel Alm on 22.03.21.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+	override func viewDidLoad() {
+		super.viewDidLoad()
+
+		// Do any additional setup after loading the view.
+	}
+
+	override var representedObject: Any? {
+		didSet {
+		// Update the view, if already loaded.
+		}
+	}
+
+
+}
+
